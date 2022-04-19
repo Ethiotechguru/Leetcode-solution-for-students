529fd4b3b45835e4ab5245337ce59b5d66f7609a
Samuel Belete <samyethio891@gmail.com>
2022-04-19 00:23:01 -0700

    longest sub String without reapiting char

---
+++ longestSubStr.js
@@ -0,0 +1,41 @@
+//Given a string s, find the length of the longest substring without repeating characters.
+const lengthOfSubstr = (s) => {
+    if(s.length<2){
+        console.log(s.length)
+        return s.length;
+    }
+	let j = 0;
+	let len = 0;
+	let currentLen = 0;
+	let str = "";
+	let currentStr = "";
+	let map = {};
+	s = s.split("");
+	while (j < s.length) {
+		if (!map[s[j]]) {
+			map[s[j]] = [s[j],j];
+			str = str + s[j];
+			len++;
+			j++;
+		} else {
+			if (len > currentLen) {
+				currentLen = len;
+				currentStr = str;
+			}
+            let indx = map[s[j]][1];
+			map = {};
+			str = "";
+			len = 0;
+			s=s.slice(indx+1);
+            j=0;
+			map[s[j]] = [s[j], j];
+			str = str + s[j];
+			len++;
+			j++;
+		}
+	}
+    currentLen = len>currentLen ? len : currentLen;
+    return currentLen;
+};
+lengthOfSubstr("au");
+//[2,3,4,5,6,7,8,9,10] 9-2 = 7
