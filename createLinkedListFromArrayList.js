a695c40a5548182df1bc49c77978ed27aacdf5a8
Samuel Belete <samyethio891@gmail.com>
2022-05-26 07:43:04 -0700

    create Linked List from array

---
+++ createLinkedListFromArrayList.js
@@ -0,0 +1,24 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+const createLinkedList = (values) => {
+	if (values.length === 0) return null;
+	let node = new Node(values[0]);
+	let dummy = node;
+	for (let i = 1; i < values.length; i++) {
+		let newNode = new Node(values[i]);
+		node.next = newNode;
+		node = node.next;
+	}
+	return dummy;
+};
+const createLinkedListRecursive = (values) => {
+  // todo
+    if(values.length === 0) return null;
+    let node = new Node(values[0]);
+    node.next = createLinkedList(values.slice(1));
+    return node;
+};
